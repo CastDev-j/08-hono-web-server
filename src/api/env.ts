47bdfe8a478bba21a110e8364633eb7c199be62e
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-09 15:44:00 -0600

    feat: update environment variable handling and remove hardcoded values from configuration

--- src/api/env.ts
+++ src/api/env.ts
@@ -1,16 +1,26 @@
 import { Hono } from "hono";
 import { env } from "hono/adapter";
 
+type Bindings = {
+  NAME: string;
+  ENV: string;
+  API_URL: string;
+  MI_API_SECRETA: string;
+};
+
 const app = new Hono();
 
-app.get("/", (c) => {
-  const environments = env<{ NAME: string; ENV: string; API_URL: string }>(c);
+app.get("/", async (c) => {
+  const environments = env<Bindings>(c);
+
+  console.log(environments);
 
   return c.text(
     `Environment Variables: 
     - The current environment is ${environments.ENV}.
     - Your name is ${environments.NAME}.
-    - The API URL is ${environments.API_URL}
+    - The API URL is ${environments.API_URL}.
+    - The secret is ${environments.MI_API_SECRETA}.
     `,
   );
 });
